dff645c8b3dc93989f40dc94f60a369052637a3a
Paul <ronquillo.paul@gmail.com>
2015-07-31 19:22:27 -0700

    added way of parsing gps info roughtly and txt serial outout from that parsing

--- code/powers combined/practice_serial_com.ino
+++ code/powers combined/practice_serial_com.ino
@@ -1,145 +1,253 @@
+int counter;
+int x,y;
+int led = 13;
+int onModulePin = 2;        // the pin to switch on the module (without press on button) 
+int8_t answer;
+ 
+char data[255];
+char latitude[11],longitude[12];
+char northSouth[1], eastWest[1];
+char date[6],UTC_time[8];
+char speed_OG[6],altitude[6];
+char gps_data[100];
 
-//Change here your data
-const char apn[] = "fast.t-mobile.com";
+const char apn[] = "*********";
 const char user_name[] = "";
 const char password[] = "";
 
 char url[ ]="gps.rubyride.co";
 int port= 80;
-char request[ ]="GET /index.php?hello=world HTTP/1.1\r\nHost: gps.rubyride.co\r\nContent-Length: 0\r\n\r\n";
+char request[ ]="GET /index.php?a=1&b=2 HTTP/1.1\r\nHost: gps.rubyride.co\r\nContent-Length: 0\r\n\r\n";
 
-
-int8_t answer;
-int onModulePin = 2, aux;
-int data_size = 0;
-int end_file = 0;
-char aux_str[50];
-char data[400];
-int http_x;
-int http_status; // 0: end, 1 waiting data with header, 2 waiting data without header, 3 timeout waiting data, 4 unknow response
-int x = 0;
-long previous;
-
-
-
+ 
 void setup(){
-  pinMode(onModulePin, OUTPUT);
-  Serial.begin(115200);
+ 
+    Serial.begin(115200);                // UART baud rate
+    delay(2000);
+    pinMode(led, OUTPUT);
+    pinMode(onModulePin, OUTPUT);
+    switchModule();                    // switches the module ON
+    delay(5000);
+ 
+    while( (sendATcommand("AT+CREG?", "+CREG: 0,1", 500) ||
+      sendATcommand("AT+CREG?", "+CREG: 0,5", 500)) == 0 );
 
-  Serial.println("Starting...");
-  power_on();
+    // sets APN, user name and password
+    sprintf(aux_str, "AT+CGSOCKCONT=1,\"IP\",\"%s\"", apn);
+    sendATcommand(aux_str, "OK", 2000);
 
-  delay(3000);
+    sprintf(aux_str, "AT+CSOCKAUTH=1,1,\"%s\",\"%s\"", user_name, password);
+    sendATcommand(aux_str, "OK", 2000);
 
-  while( (sendATcommand("AT+CREG?", "+CREG: 0,1", 500) ||
-    sendATcommand("AT+CREG?", "+CREG: 0,5", 500)) == 0 );
-
-  // set APN
-  sprintf(aux_str, "AT+CGSOCKCONT=1,\"IP\",\"%s\"", apn);
-  sendATcommand(aux_str, "OK", 2000);
-
-  // Set username and password
-  sprintf(aux_str, "AT+CSOCKAUTH=1,1,\"%s\",\"%s\"", user_name, password);
-  sendATcommand(aux_str, "OK", 2000);
-
-  delay(3000);
-
-  // starts GPS session in stand alone mode
-  answer = sendATcommand("AT+CGPS=1,1","OK",1000);    
-  if (answer == 0)
-  {
-    Serial.println("Error starting the GPS");
-    Serial.println("The code stucks here!!");
-    while(1);
-  }
-}
-
-
-
-void loop(){
-
-  // request the url
-  previous = millis();
-  sprintf(aux_str, "AT+CHTTPACT=\"%s\",%d", url, port);
-  answer = sendATcommand(aux_str, "+CHTTPACT: REQUEST", 60000);
-
-  // Sends the request
-  Serial.println(request);
-  // Sends <Ctrl+Z>
-  Serial.write(0x1A);
-
-  previous = millis() - previous;
-  
-  Serial.print(F("previous in DEC: "));
-  Serial.println(previous, DEC);
-  
-
-  delay(10000);
-
-}
-
-
-
-void power_on(){
-
-  uint8_t answer=0;
-
-  // checks if the module is started
-  answer = sendATcommand("AT", "OK", 2000);
-  if (answer == 0)
-  {
-    // power on pulse
-    digitalWrite(onModulePin,HIGH);
+    answer = sendATcommand("AT+CGPS=1,1","OK",1000);    
+    if (answer == 0)
+        {
+            Serial.println("Error starting the GPS");
+            Serial.println("The code stucks here!!");
+            while(1);
+        }
     delay(3000);
+}
+ 
+void loop(){
+ 
+    delay(5000);
+    // Serial.println("AT+CGPSINFO"); // request GPS info
+    // Serial.flush(); // wait to finish the outgoing data before moving forward
+    answer = sendATcommand("AT+CGPSINFO","+CGPSINFO:",1000);    // request info from GPS
+
+    // Read GPS data and store in variables or error
+    readGpsData();
+
+    if(answer == ){
+        Serial.print("latitude : ");
+        Serial.println(latitude);
+    
+        Serial.print("northSouth : ");
+        Serial.println(northSouth);
+        
+        Serial.print("longitude : ");
+        Serial.println(longitude);
+    
+        Serial.print("eastWest : ");
+        Serial.println(eastWest);
+        
+        Serial.print("date : ");
+        Serial.println(date);
+        
+        Serial.print("UTC_time : ");
+        Serial.println(UTC_time);
+        
+        Serial.print("altitude : ");
+        Serial.println(altitude);
+        
+        Serial.print("speed_OG : ");
+        Serial.println(speed_OG);
+        
+        Serial.println("");
+    }
+}
+
+
+// TUrns cookinghacks module on.
+void switchModule(){
+    digitalWrite(onModulePin,HIGH);
+    delay(2000);
     digitalWrite(onModulePin,LOW);
 
-    // waits for an answer from the module
-    while(answer == 0){
-      // Send AT every two seconds and wait for the answer
-      answer = sendATcommand("AT", "OK", 2000);
-    }
-  }
+    uint8_t answer=0;
 
+    // checks if the module is started
+    answer = sendATcommand("AT", "OK", 2000);
+    if (answer == 0)
+    {
+        // power on pulse
+        digitalWrite(onModulePin,HIGH);
+        delay(3000);
+        digitalWrite(onModulePin,LOW);
+
+        // waits for an answer from the module
+        while(answer == 0){    
+            // Send AT every two seconds and wait for the answer
+            answer = sendATcommand("AT", "OK", 2000);    
+        }
+    }
 }
 
-int8_t sendATcommand(char* ATcommand, char* expected_answer1, unsigned int timeout) {
-
-  uint8_t x = 0,  answer = 0;
-  char response[100];
-  unsigned long previous;
-
-  memset(response, '\0', 100);    // Initialize the string
-
-  delay(100);
-
-  while ( Serial.available() > 0) Serial.read();   // Clean the input buffer
-
-  Serial.println(ATcommand);    // Send the AT command
 
 
-  x = 0;
-  previous = millis();
-
-  // this loop waits for the answer
-  do {
-    // if there are data in the UART input buffer, reads it and checks for the asnwer
-    if (Serial.available() != 0) {
-      response[x] = Serial.read();
-      x++;
-      // check if the desired answer is in the response of the module
-      if (strstr(response, expected_answer1) != NULL)
-      {
-        answer = 1;
-      }
+//reads GPS data and stores it in char array
+// parse gps must come after
+void readGpsData(){
+    // counter used for stepping through the data char
+    // x=0;
+    // do{
+    //     do{
+    //         digitalWrite(led,HIGH);  
+    //     }
+    //     while(Serial.available()==0);
+    //     digitalWrite(led,LOW);
+    //     data[x]=Serial.read();  
+    //     x++;                    
+    // }
+    // while(Serial.read()!='K');
+    if (answer == 1){
+        counter = 0;
+        do{
+          while(Serial.available() == 0);
+          gps_data[counter] = Serial.read();
+          counter++;
+        }
+        while(gps_data[counter - 1] != '\r');
+        gps_data[counter] = '\0';
+        Serial.println("Parsing...");
+        parseGpsOrErr();
     }
-    // Waits for the asnwer with time out
-  }
-  while ((answer == 0) && ((millis() - previous) < timeout));
-
-  return answer;
+    else{
+        Serial.println("Error, no answer");
+    }
 }
 
-int8_t sendATcommand2(char* ATcommand, char* expected_answer1, char* expected_answer2, unsigned int timeout)
+
+/*
+ * Parses the GPS string into seperate variables or returns an error.
+ * This must come right after read gps data function
+ * vars used:
+ *   latitude
+ *   longitude
+ *   date
+ *   UTC time
+ *   altitude
+ *   speed
+ *   knots
+ */
+void parseGpsOrErr(){
+    x=0;
+    y=0;
+    Serial.print("char at pos 0__");
+    Serial.print(gps_data[x]);
+    Serial.println("__");
+    if(gps_data[x]!=','){
+      // stores latitude
+       do{
+           latitude[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++;
+
+       // stores northSouth direction
+       y=0;
+       northSouth[y] = gps_data[x];
+
+       x+=2;
+       y=0;
+       // stores longitude
+       do{
+           longitude[y]=gps_data[x];
+           y++;
+           x++; 
+       }
+       while(gps_data[x]!=',');
+
+
+       x++;
+
+       // stores eastWest direction
+       y=0;
+       eastWest[y] = gps_data[x];
+
+       x+=2;
+       y=0;
+       //stores the date
+       do{
+           date[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++;
+
+       y=0;
+       //stores TC time
+       do{
+           UTC_time[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++;
+
+       y=0;
+       // stores altitude
+       do{
+           altitude[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++; 
+
+       y=0;
+       // store speed
+       do{
+           speed_OG[y]=gps_data[x];
+           // Serial.print(gps_data[x]);
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=0x0D);
+       Serial.println("Done Parsing...");
+    }
+    else{
+        Serial.println("GPS information not available, please wait...");
+    }   
+}  
+
+
+// Sends an AT commands and waits for a response with a timeout
+int8_t sendATcommand(char* ATcommand, char* expected_answer1, unsigned int timeout)
 {
 
   uint8_t x=0,  answer=0;
@@ -150,9 +258,9 @@ int8_t sendATcommand2(char* ATcommand, char* expected_answer1, char* expected_an
 
   delay(100);
 
- // while( Serial.available() > 0) Serial.read();    // Clean the input buffer
+  while( Serial.available() > 0) Serial.read();    // Clean the input buffer
 
-  Serial.println(ATcommand);    // Send the AT command
+  Serial.println(ATcommand);    // Send the AT command 
 
 
     x = 0;
@@ -161,77 +269,18 @@ int8_t sendATcommand2(char* ATcommand, char* expected_answer1, char* expected_an
   // this loop waits for the answer
   do{
 
-    if(Serial.available() != 0){
+    if(Serial.available() != 0){    
       response[x] = Serial.read();
       x++;
       // check if the desired answer is in the response of the module
-      if (strstr(response, expected_answer1) != NULL)
+      if (strstr(response, expected_answer1) != NULL)    
       {
         answer = 1;
       }
-      // check if the desired answer is in the response of the module
-      if (strstr(response, expected_answer2) != NULL)
-      {
-        answer = 2;
-      }
     }
-    // Waits for the asnwer with time out
+    // Waits for the answer with time out
   }
-  while((answer == 0) && ((millis() - previous) < timeout));
+  while((answer == 0) && ((millis() - previous) < timeout));    
 
   return answer;
-} 
-
-
-/*
- * This function takes the gps data and builds an http request
-*/
-void buildRequest()
-{
-  digitalWrite(13, HIGH);
-  //
-}
-
-/*
- * This function sorts out the GPS data
-*/
-void parseGPS()
-{
-  digitalWrite(13, HIGH);
-  //
-}
-
-void gps version main loop
-{
-  answer = sendATcommand("AT+CGPSINFO","+CGPSINFO:",1000);    // request info from GPS
-  if (answer == 1)
-  {
-
-    counter = 0;
-    do{
-      while(Serial.available() == 0);
-      gps_data[counter] = Serial.read();
-      counter++;
-    }
-    while(gps_data[counter - 1] != '\r');
-    gps_data[counter] = '\0';
-    if(gps_data[0] == ',')
-    {
-      Serial.println("No GPS data available");
-      Serial.println("");
-    }
-    else
-    {
-      Serial.print("GPS data:");
-      Serial.print(gps_data);  
-      Serial.println("");
-    }       
-
-  }
-  else
-  {
-    Serial.println("Error"); 
-  }
-
-  delay(5000);
 }
--- code/tutorial examples/standalone_gps_alt/standalone_gps_alt.ino
+++ code/tutorial examples/standalone_gps_alt/standalone_gps_alt.ino
@@ -1,14 +1,15 @@
-
- 
+int counter;
+int x,y;
 int led = 13;
 int onModulePin = 2;        // the pin to switch on the module (without press on button) 
+int8_t answer;
  
 char data[255];
 char latitude[11],longitude[12];
+char northSouth[1], eastWest[1];
 char date[6],UTC_time[8];
 char speed_OG[6],altitude[6];
- 
-int x,y;
+char gps_data[100];
 
  
 void setup(){
@@ -18,38 +19,57 @@ void setup(){
     pinMode(led, OUTPUT);
     pinMode(onModulePin, OUTPUT);
     switchModule();                    // switches the module ON
+    delay(5000);
  
-    for (int i=0;i< 5;i++){
-        delay(5000);
-    } 
- 
-    Serial.println("AT+CGPS=1,1");         // starts GPS session in stand-alone mode
+    // Serial.println("AT+CGPS=1,1");         // starts GPS session in stand-alone mode
+    // uses the checked send at command function to start the GPS session
+    answer = sendATcommand("AT+CGPS=1,1","OK",1000);    
+    if (answer == 0)
+        {
+            Serial.println("Error starting the GPS");
+            Serial.println("The code stucks here!!");
+            while(1);
+        }
     delay(100);
 }
  
 void loop(){
  
     delay(5000);
-    Serial.println("AT+CGPSINFO"); // request GPS info
-    Serial.flush();
-    
-    // counter used for stepping through the data char
-    x=0;
+    // Serial.println("AT+CGPSINFO"); // request GPS info
+    // Serial.flush(); // wait to finish the outgoing data before moving forward
+    answer = sendATcommand("AT+CGPSINFO","+CGPSINFO:",1000);    // request info from GPS
 
-    // Read GPS data and end on a 'K' when the character should be a comma following
+    // Read GPS data and store in variables or error
     readGpsData();
- 
-    x=24;
-    y=0;
-    parseGpsOrErr();
 
-    // print longitude var
-    Serial.print("Lon var = ");
-    Serial.print(longitude);
-
-    // print longitude var
-    Serial.print("Lon var = ");
-    Serial.print(longitude);
+    if(answer == ){
+        Serial.print("latitude : ");
+        Serial.println(latitude);
+    
+        Serial.print("northSouth : ");
+        Serial.println(northSouth);
+        
+        Serial.print("longitude : ");
+        Serial.println(longitude);
+    
+        Serial.print("eastWest : ");
+        Serial.println(eastWest);
+        
+        Serial.print("date : ");
+        Serial.println(date);
+        
+        Serial.print("UTC_time : ");
+        Serial.println(UTC_time);
+        
+        Serial.print("altitude : ");
+        Serial.println(altitude);
+        
+        Serial.print("speed_OG : ");
+        Serial.println(speed_OG);
+        
+        Serial.println("");
+    }
 }
 
 
@@ -58,26 +78,64 @@ void switchModule(){
     digitalWrite(onModulePin,HIGH);
     delay(2000);
     digitalWrite(onModulePin,LOW);
+
+    uint8_t answer=0;
+
+    // checks if the module is started
+    answer = sendATcommand("AT", "OK", 2000);
+    if (answer == 0)
+    {
+        // power on pulse
+        digitalWrite(onModulePin,HIGH);
+        delay(3000);
+        digitalWrite(onModulePin,LOW);
+
+        // waits for an answer from the module
+        while(answer == 0){    
+            // Send AT every two seconds and wait for the answer
+            answer = sendATcommand("AT", "OK", 2000);    
+        }
+    }
 }
 
 
+
 //reads GPS data and stores it in char array
+// parse gps must come after
 void readGpsData(){
-    do{
+    // counter used for stepping through the data char
+    // x=0;
+    // do{
+    //     do{
+    //         digitalWrite(led,HIGH);  
+    //     }
+    //     while(Serial.available()==0);
+    //     digitalWrite(led,LOW);
+    //     data[x]=Serial.read();  
+    //     x++;                    
+    // }
+    // while(Serial.read()!='K');
+    if (answer == 1){
+        counter = 0;
         do{
-            digitalWrite(led,HIGH);  
+          while(Serial.available() == 0);
+          gps_data[counter] = Serial.read();
+          counter++;
         }
-        while(Serial.available()==0);
-        digitalWrite(led,LOW);
-        data[x]=Serial.read();  
-        x++;                        
+        while(gps_data[counter - 1] != '\r');
+        gps_data[counter] = '\0';
+        Serial.println("Parsing...");
+        parseGpsOrErr();
     }
-    while(Serial.read()!='K');
+    else{
+        Serial.println("Error, no answer");
+    }
 }
 
 
 /*
  * Parses the GPS string into seperate variables or returns an error.
+ * This must come right after read gps data function
  * vars used:
  *   latitude
  *   longitude
@@ -88,82 +146,125 @@ void readGpsData(){
  *   knots
  */
 void parseGpsOrErr(){
-    if(data[x]!=','){
-        Serial.print("Latitude: ");    //shows actual latitude
-        do{
-            latitude[y]=data[x];
-            Serial.print(data[x]);
-            y++;
-            x++;        
-        }
-        while(data[x]!=',');
-        x++;
-        Serial.print(" ");
-        Serial.println(data[x]);    //north or south
- 
-        x+=2;
-        y=0;
-        Serial.print("Longitude: ");    //shows actual longitude
-        do{
-            longitude[y]=data[x];
-            Serial.print(data[x]);
-            y++;
-            x++; 
-        }
-        while(data[x]!=',');
+    x=0;
+    y=0;
+    Serial.print("char at pos 0__");
+    Serial.print(gps_data[x]);
+    Serial.println("__");
+    if(gps_data[x]!=','){
+      // stores latitude
+       do{
+           latitude[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++;
+
+       // stores northSouth direction
+       y=0;
+       northSouth[y] = gps_data[x];
+
+       x+=2;
+       y=0;
+       // stores longitude
+       do{
+           longitude[y]=gps_data[x];
+           y++;
+           x++; 
+       }
+       while(gps_data[x]!=',');
 
 
-        x++;
-        Serial.print(" ");
-        Serial.println(data[x]);    //west or east
- 
-        x+=2;
-        y=0;
-        Serial.print("Date: ");    //shows date
-        do{
-            date[y]=data[x];
-            Serial.print(data[x]);
-            y++;
-            x++;        
-        }
-        while(data[x]!=',');
-        x++;
- 
-        y=0;
-        Serial.print("\r\nUTC time: ");    //shows UTC time
-        do{
-            UTC_time[y]=data[x];
-            Serial.print(data[x]);
-            y++;
-            x++;        
-        }
-        while(data[x]!=',');
-        x++;
- 
-        y=0;
-        Serial.print("\r\nAltitude: ");    //shows altitude
-        do{
-            UTC_time[y]=data[x];
-            Serial.print(data[x]);
-            y++;
-            x++;        
-        }
-        while(data[x]!=',');
-        Serial.println(" m");
-        x++; 
- 
-        y=0;
-        Serial.print("Speed: ");    //shows speed
-        do{
-            speed_OG[y]=data[x];
-            Serial.print(data[x]);
-            y++;
-            x++;        
-        }
-        while(data[x]!=0x0D);
-        Serial.println(" knots");
+       x++;
+
+       // stores eastWest direction
+       y=0;
+       eastWest[y] = gps_data[x];
+
+       x+=2;
+       y=0;
+       //stores the date
+       do{
+           date[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++;
+
+       y=0;
+       //stores TC time
+       do{
+           UTC_time[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++;
+
+       y=0;
+       // stores altitude
+       do{
+           altitude[y]=gps_data[x];
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=',');
+       x++; 
+
+       y=0;
+       // store speed
+       do{
+           speed_OG[y]=gps_data[x];
+           // Serial.print(gps_data[x]);
+           y++;
+           x++;        
+       }
+       while(gps_data[x]!=0x0D);
+       Serial.println("Done Parsing...");
     }
     else{
         Serial.println("GPS information not available, please wait...");
     }   
 }  
+
+
+// Sends an AT commands and waits for a response with a timeout
+int8_t sendATcommand(char* ATcommand, char* expected_answer1, unsigned int timeout)
+{
+
+  uint8_t x=0,  answer=0;
+  char response[100];
+  unsigned long previous;
+
+  memset(response, '\0', 100);    // Initialize the string
+
+  delay(100);
+
+  while( Serial.available() > 0) Serial.read();    // Clean the input buffer
+
+  Serial.println(ATcommand);    // Send the AT command 
+
+
+    x = 0;
+  previous = millis();
+
+  // this loop waits for the answer
+  do{
+
+    if(Serial.available() != 0){    
+      response[x] = Serial.read();
+      x++;
+      // check if the desired answer is in the response of the module
+      if (strstr(response, expected_answer1) != NULL)    
+      {
+        answer = 1;
+      }
+    }
+    // Waits for the answer with time out
+  }
+  while((answer == 0) && ((millis() - previous) < timeout));    
+
+  return answer;
+}
